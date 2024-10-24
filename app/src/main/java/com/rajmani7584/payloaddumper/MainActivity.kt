663f4f2cb7d3354181d6c01cd1e838a4939ce17f
rajmani7584 <rajmani7584@gmail.com>
2024-10-24 15:57:10 +0530

    Initial commit (Project in Development)

---
+++ app/src/main/java/com/rajmani7584/payloaddumper/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.rajmani7584.payloaddumper
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.rajmani7584.payloaddumper.ui.theme.PayloadDumperAndroidTheme
+
+class MainActivity : ComponentActivity() {
+
+    private external fun getPartitionList(name: String): String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PayloadDumperAndroidTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column (Modifier.padding(innerPadding)) {
+                        Text(getPartitionList("/sdcard/payload.bin"))
+                    }
+                }
+            }
+        }
+    }
+
+    companion object {
+        init {
+            System.loadLibrary("payload-dumper-rust")
+        }
+    }
+}
