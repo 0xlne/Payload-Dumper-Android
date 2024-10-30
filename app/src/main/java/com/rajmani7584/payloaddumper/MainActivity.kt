9ed24bc226acc9ea15edfba4e82bea748adb4fcb
rajmani7584 <rajmani7584@gmail.com>
2024-10-30 20:04:44 +0530

    working build

--- app/src/main/java/com/rajmani7584/payloaddumper/MainActivity.kt
+++ app/src/main/java/com/rajmani7584/payloaddumper/MainActivity.kt
@@ -1,35 +1,172 @@
 package com.rajmani7584.payloaddumper
 
 import android.os.Bundle
+import android.os.Environment
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.rajmani7584.payloaddumper.ui.theme.PayloadDumperAndroidTheme
+import java.io.File
+import kotlin.concurrent.thread
 
 class MainActivity : ComponentActivity() {
 
-    private external fun getPartitionList(name: String): String
+    private external fun getPartitionList(path: String): String
+    private external fun extractPartition(path: String, partition: String, outputPath: String): String
+    val externalStoragePath = Environment.getExternalStorageDirectory().absolutePath
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val appDirectory = File("$externalStoragePath/PayloadDumperAndroid")
+        if (!appDirectory.exists()) appDirectory.mkdir()
+        if (!appDirectory.isDirectory) {
+            appDirectory.deleteRecursively()
+            appDirectory.mkdir()
+        }
         setContent {
+            val selectingPayload = remember { mutableStateOf(false) }
+            var partitionsList by remember { mutableStateOf(emptyList<Pair<String, Float>>()) }
+            val typeDir = remember { mutableStateOf(false) }
+            val fileToExtract = remember { mutableStateOf("") }
+            val outputDirectory = remember { mutableStateOf("$externalStoragePath/PayloadDumperAndroid") }
+            var listOutput by remember { mutableStateOf("") }
+
+            LaunchedEffect(fileToExtract.value) {
+                if (fileToExtract.value.isEmpty()) return@LaunchedEffect
+                val output = getPartitionList(fileToExtract.value)
+                if (output.startsWith("Err:")) {
+                    listOutput = output
+                    return@LaunchedEffect
+                }
+
+                // Parsing "partitionName|sizeInMB" format
+                partitionsList = output.removePrefix("Partitions:").trim().split(",").mapNotNull { partition ->
+                    val parts = partition.split("|")
+                    if (parts.size == 2) {
+                        val name = parts[0].trim()
+                        val size = parts[1].toFloatOrNull()?.div(1000) ?: 0f
+                        name to size
+                    } else null
+                }
+            }
+
             PayloadDumperAndroidTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column (Modifier.padding(innerPadding)) {
-                        Text(getPartitionList("/sdcard/payload.bin"))
+                    Column(
+                        Modifier
+                            .padding(innerPadding)
+                            .padding(14.dp)
+                            .fillMaxSize()
+                    ) {
+                        Row {
+                            Box(Modifier.width(0.dp).weight(1f)) {
+                                Text(if (fileToExtract.value.isEmpty()) "Select payload to extract" else fileToExtract.value, fontSize = 16.sp)
+                            }
+                                Button(onClick = {
+                                    selectingPayload.value = true
+                                    typeDir.value = false
+                                }) { Text("Select Payload") }
+                        }
+                        Spacer(Modifier.height(4.dp))
+                        Row {
+                            Box(Modifier.width(0.dp).weight(1f)) {
+                                Text(outputDirectory.value, fontSize = 16.sp)
+                            }
+                                Button(onClick = {
+                                    selectingPayload.value = true
+                                    typeDir.value = true
+                                }) { Text("Select Directory") }
+                        }
+
+                        if (listOutput.startsWith("Err:")) {
+                            Text(listOutput)
+                        }
+                        else {
+                            LazyColumn(
+                                Modifier
+                                    .fillMaxWidth()
+                                    .padding(18.dp)
+                                    .background(Color(0xFFEEE4A3))
+                            ) {
+                                items(partitionsList.size) { index ->
+                                    val (partitionName, partitionSize) = partitionsList[index]
+                                    Row(verticalAlignment = Alignment.CenterVertically) {
+                                        var status by remember { mutableStateOf("status") }
+                                        Text(partitionName, fontSize = 22.sp)
+                                        Spacer(Modifier.width(8.dp))
+                                        Text(status)
+                                        Spacer(Modifier.weight(1f))
+                                        Text("$partitionSize KB", fontSize = 22.sp)
+                                        Spacer(Modifier.width(8.dp))
+                                        Button(onClick = {
+                                            thread {
+                                                status = "Extracting..."
+                                                val res = extractPartition(
+                                                    fileToExtract.value,
+                                                    partitionName,
+                                                    "${outputDirectory.value}/$partitionName.img"
+                                                )
+                                                status = res
+                                            }
+                                        }) {
+                                            Text("Extract")
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                        val dirPath = externalStoragePath
+                        val currentPath = remember { mutableStateOf(dirPath) }
+                        if (selectingPayload.value)
+                            SelectPayload(
+                                typeDir, currentPath, dirPath,
+                                outputDirectory,
+                                fileToExtract,
+                                selectingPayload,
+                                Modifier
+                                    .height((resources.displayMetrics.heightPixels / 2).dp)
+                                    .width((resources.displayMetrics.widthPixels / 2).dp)
+                                    .background(
+                                        Color.White,
+                                        shape = RoundedCornerShape(16.dp)
+                                    )
+                            )
                     }
                 }
             }
         }
+
     }
 
     companion object {
--- app/src/main/java/com/rajmani7584/payloaddumper/SelectPayload.kt
+++ app/src/main/java/com/rajmani7584/payloaddumper/SelectPayload.kt
@@ -1,4 +1,187 @@
-package com.rajmani7584.payloaddumper.ui.theme
+package com.rajmani7584.payloaddumper
 
-class SelectPayload {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.io.File
+
+@Composable
+fun SelectPayload(
+    typeDir: MutableState<Boolean>,
+    currentPath: MutableState<String>,
+    dirPath: String,
+    outputDirectory: MutableState<String>,
+    payload: MutableState<String>,
+    selectingPayload: MutableState<Boolean>,
+    modifier: Modifier
+) {
+    val fileModifier = Modifier
+        .padding(7.dp)
+        .height(45.dp)
+        .fillMaxWidth()
+        .background(Color(0x33aaaaaa), shape = RoundedCornerShape(12.dp))
+
+    // Use SnapshotStateList for incremental loading
+    val fileList = remember { mutableStateListOf<String>() }
+    var canGoBack by remember { mutableStateOf(true) }
+
+    LaunchedEffect(currentPath.value) {
+        canGoBack = currentPath.value.length > dirPath.length
+        fileList.clear()
+
+        withContext(Dispatchers.IO) {
+            val allFiles = File(currentPath.value).listFiles()
+            if (allFiles != null) {
+                val folders = mutableListOf<String>()
+                val files = mutableListOf<String>()
+
+                allFiles.forEach { file ->
+                    if (file.isDirectory) {
+                        folders.add(file.name)
+                    } else if (!typeDir.value && file.extension == "bin") {
+                        files.add("fl:${file.name}")
+                    }
+                }
+                fileList += folders + files
+            }
+        }
+    }
+
+    Dialog(onDismissRequest = { selectingPayload.value = false }) {
+        Column(modifier) {
+            Text(
+                text = "Select ${if (typeDir.value) "Directory to Extract" else "Payload.bin"}:",
+                fontSize = 20.sp,
+                modifier = Modifier.padding(horizontal = 15.dp, vertical = 8.dp)
+            )
+            Text("> ${currentPath.value}", modifier = Modifier.padding(start = 15.dp))
+
+            if (canGoBack) {
+                Row(
+                    modifier = fileModifier.clickable {
+                        currentPath.value = File(currentPath.value).parent ?: currentPath.value
+                    },
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text("Parent directory", fontSize = 18.sp, modifier = Modifier.padding(start = 15.dp))
+                }
+            }
+
+            LazyColumn(
+                Modifier
+                    .fillMaxWidth()
+                    .weight(1f), horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                if (fileList.isEmpty()) {
+                    item { Text(if (File(currentPath.value).canRead()) "No files" else "No permission") }
+                } else {
+                    items(fileList.size) {
+                        val file = fileList[it]
+                        if (typeDir.value) {
+                            FolderButton(file, currentPath, fileModifier)
+                        } else {
+                            if (file.startsWith("fl:")) {
+                                FileButton(file, fileModifier.clickable(true, onClick = {
+                                    payload.value = "${currentPath.value}/${file.removePrefix("fl:")}"
+                                    selectingPayload.value = false
+                                }))
+                            } else {
+                                FolderButton(file, currentPath, fileModifier)
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (typeDir.value) {
+                Row(
+                    Modifier
+                        .fillMaxWidth()
+                        .height(60.dp),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    Box(
+                        Modifier
+                            .fillMaxHeight()
+                            .padding(10.dp)
+                            .background(Color(0x336666ff), shape = RoundedCornerShape(6.dp))
+                            .clickable {
+                                outputDirectory.value = currentPath.value
+                                selectingPayload.value = false
+                            }
+                    ) {
+                        Text(if (File(currentPath.value).canWrite()) "Extract Here" else "No Write Access", fontSize = 18.sp, modifier = Modifier.padding(8.dp))
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FileButton(
+    file: String,
+    modifier: Modifier
+) {
+    Row(
+        modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            imageVector = ImageVector.vectorResource(R.drawable.baseline_file_present_24),
+            contentDescription = null,
+            modifier = Modifier.padding(horizontal = 8.dp)
+        )
+        Text(file.removePrefix("fl:"), fontSize = 18.sp)
+    }
+}
+
+@Composable
+fun FolderButton(
+    file: String,
+    currentPath: MutableState<String>,
+    modifier: Modifier
+) {
+    Row(
+        modifier.clickable {
+            currentPath.value = "${currentPath.value}/${file}"
+        },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Image(
+            imageVector = ImageVector.vectorResource(id = R.drawable.baseline_folder_24),
+            contentDescription = null,
+            modifier = Modifier.padding(horizontal = 8.dp)
+        )
+        Text(file, fontSize = 18.sp)
+    }
 }
